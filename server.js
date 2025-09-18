d50beb2b350a1366f67f1c8b4ec761a47cb939ac
Khusan Abdukodirov <khusan@gmail.com>
2025-09-18 19:13:29 +0900

    fix qilingani

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 console.log("Web Serverni boshlash");
-const express = require("express");
+const express = require('express');
 const app = express();
 const http = require("http");
 
-// 1 : KIRISH
+// 1: Kirish code
 app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
-// 2: Session related
-
-// 3 : Views related
+// 2: Session code
+// 3: Views code
 app.set("views", "views");
-app.set("view engine", "ejs");
+app.set("views engine", "ejs");
 
-
-
-// 4 ROUTING related
-app.get("/hello", function(req, res) {
-    res.end(`<h1>HELLO WORLD</h1>`);
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
 });
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
+  console.log(`Server ${PORT} portda ishga tushdi`);
 });
