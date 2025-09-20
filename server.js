7487679fafe20659005642d7c218c48fbb122835
Khusan Abdukodirov <khusan@gmail.com>
2025-09-20 18:24:01 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 console.log("Web Serverni boshlash");
 const express = require('express');
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { error } = require('console');
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if(error) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,13 +27,17 @@ app.set("view engine", "ejs");
 
 // 4: Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
 });
+
+app.get('/author', (req, res) => {
+  res.render("author", { user: user });
+});
+
 
 app.get("/", function (req, res) {
   res.render("harid");
 });
+
 
 const server = http.createServer(app);
 let PORT = 3000;
