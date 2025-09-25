d079c2a5ab6fe0d08fa2b8b67a1594cfbd5a1778
Khusan Abdukodirov <khusan@gmail.com>
2025-09-25 19:26:26 +0900

    feat:CRud operatsiaylari va B TAsk

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-
+// MIT A _ TASK
 // function countLetter(letter, emergency) {
 //   let count = 0;
 //   for (let i = 0; i < emergency.length; i++) {
@@ -11,130 +11,16 @@
 
 // console.log(countLetter("e", "emergency")); 
 
-// NODE JS event  loop va CALLBACK functionlarni organamz!
+// MIT B TASK
 
-// console.log("Jack Ma maslahatlari");
-// const list = [
-//     "yaxshi talaba boling", // 0-20
-//     "togri boshliq tanlang va koproq xato qiling", // 20-30
-//     "uzingizga ishlashingizni boshlang", // 30-40
-//     "siz kuchli bolgan narsalarni qiling", // 40-50
-//     "yoshlarga investitsiya qiling", // 50-60
-//     "endi dam oling, foydasi yoq endi", // 60
-// ];
- 
-// function maslahatBering(a, callback) {
-//   if(typeof a !== "number") callback("insert number", null);
-//   else if(a <= 20) callback(null, list[0]);
-//   else if(a > 20 && a <= 30) callback(null, list[1]);
-//   else if(a > 30 && a <= 40) callback(null, list[2]);
-//   else if(a > 40 && a <= 50) callback(null, list[3]);
-//   else if(a > 50 && a <= 60) callback(null, list[4]);
-//   else {
-//     setTimeout(function() {
-//       callback(null, list[5]) 
-//     }, 5000);
-//   }
-// }
-
-// console.log("passed here 0");
-// maslahatBering(65, (err, data) => {
-//     if(err) console.log('ERROR', err);
-//     console.log('javob:', data);           
-// });
-// console.log("passed here 1");
-
-
-//--------------------------------------------------------------------------------------------------//
-
-
-// ASYNCHRONOUS functionalrni qollash!!!
-
-// console.log("Jack Ma maslahatlari");
-// const list = ["yaxshi talaba boling", // 0-20
-// "togri boshliq tanlang va koproq xato qiling", // 20-30
-// "uzingizga ishlashingizni boshlang", // 30-40
-// "siz kuchli bolgan narsalarni qiling", // 40-50
-// "yoshlarga investitsiya qiling", // 50-60
-// "endi dam oling, foydasi yoq endi", // 60
-// ];
-
-// // console.log("Jack Ma maslahatlari");
-// // const list = [
-// // ];
- 
-// function maslahatBering(a, callback) {
-//   if(typeof a !== "number") callback("insert number", null);
-//   else if(a <= 20) callback(null, list[0]);
-//   else if(a > 20 && a <= 30) return list[1];
-//   else if(a > 30 && a <= 40) return list[2];
-//   else if(a > 40 && a <= 50) return list[3];
-//   else if(a > 50 && a <= 60) return list[4];
-//   else {
-//     setInterval(function () {
-//           callback(null, list[5]);
-//     }, 1000);
-//   };
-//  }
-
-//  console.log("passed here 0");
-// maslahatBering(70, (err, data) => {
-//     if(err) console.log('ERROR', err);
-//     else {
-//         console.log(data);
-//     }           
-// });
-// console.log("passed here 1");
-
-
-//  call via then/catch
-// console.log("passed here 0");
-// maslahatBering(25)
-// .then(data => {
-//   console.log('javob:', data);
-// })
-// .catch(err => {
-//   console.log('ERROR:', err);
-// });
-// console.log("passed here 1");
-
-
-// call via asnyc/await
-// async function run() {
-//     let javob = await maslahatBering(25);
-//     console.log(javob);
-//     javob = await maslahatBering(70);
-//     console.log(javob);
-//      javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-// run();
-
-
-
-// ======================================================//
-
-// MI TASK 
-
-// A-TASK: countLetter function
-
-
-// DEFINE
-function qoldiqliBolish(a, b, callback) {
-  if (b === 0) {
-    callback("Mahraj nolga teng emas!", null);
-  } else {
-    const c = a % b;
-    callback(null, c);
+function countDigits(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i]) && str[i] !== " ") {
+      count++;
+    }
   }
+  return count;
 }
 
-// CALL
-qoldiqliBolish(7, 5, (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    console.log("data:", data);
-    console.log("MANTIQLAR...");
-  }
-});
+console.log(countDigits("SoccerR7R9M10j21u1"));
