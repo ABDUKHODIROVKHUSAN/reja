6583d3e2bf898d52ef0df7ef9c92f357c40da51b
Khusan Abdukodirov <khusan@gmail.com>
2025-09-27 00:52:03 +0900

    feat:publishing toliq yakunlandi

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
+console.log("Web Serverni boshlash");
+const express = require('express');
+const res = require("express/lib/response");
+const app = express();
 const http = require("http");
-const mongodb = require("mongodb");
+const fs = require("fs");
 
-let db;
-const connectionString = "mongodb+srv://officialabdukhodirov_db_user:6mj9J915j68HrMkk@cluster0.g6dpcom.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster"
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,
-}, (err, client) => {
-  if(err) console.log("ERROR on connection MongoDB");
-  else {
-    console.log("MongoDB connection succeed");
-    module.exports = client;
-
-    const app = require("./app");
-    const server = http.createServer(app);
-    let PORT = 3000;
-    server.listen(PORT, function () {
-    console.log(
-    `The server is running successfully on port ${PORT}, http:/.localhost:${PORT}`
-  );
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+if(err) {
+  console.log("ERROR:", err);
+} else {
+  user = JSON.parse(data)
+}
 });
-  }
-});  
 
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+  //to do code with db here
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+})
+
+
+app.get("/", function (req, res) {
+      res.render("harid");
+});
+
+     const server = http.createServer(app);
+     let PORT = 3000;
+     server.listen(PORT, function () {
+     console.log(`The server is running successfully on port ${PORT}`);
+     });
